49f8c2042b2847d5a2e94910ad5cb904cbb7ab46
Alex Ostrovski <ostrovski.alex@gmail.com>
2020-12-20 14:24:57 +0200

    Use transform module in CLI

--- examples/cli.rs
+++ examples/cli.rs
@@ -1,11 +1,17 @@
 //! CLI for rendering Julia sets to a file.
 
 use anyhow::anyhow;
+use image::{Luma, Rgb};
 use structopt::StructOpt;
 
 use std::{path::PathBuf, str::FromStr};
 
-use julia_set::{Backend, Function, ImageBuffer, Params, Render};
+use julia_set::{
+    transform::{ApplyTransform, Negative, Palette, PixelTransform, Smoothstep},
+    Backend, Function, ImageBuffer, Params, Render,
+};
+
+type LumaTransform = dyn PixelTransform<Luma<u8>, Output = Luma<u8>>;
 
 const ABOUT: &str = "CLI for rendering a Julia set to a file.";
 
@@ -39,12 +45,30 @@ struct Args {
     #[structopt(name = "iter", long, default_value = "100")]
     max_iterations: u8,
 
+    /// List of luma transforms.
+    #[structopt(name = "tr", long, default_value = "")]
+    transforms: NamedLumaTransforms,
+    /// Palette to use. If not specified, the image will be grayscale.
+    #[structopt(name = "palette", long, short = "p")]
+    palette: Option<NamedPalette>,
+
     /// Complex-valued function for the Julia set, for example, "z * z + 0.5 - 0.4i".
     #[structopt(name = "function")]
     function: String,
 }
 
 impl Args {
+    fn luma_transform(&self) -> Box<LumaTransform> {
+        match self.transforms.0.as_slice() {
+            [] => Box::new(()),
+            [x] => x.to_pixel_transform(),
+            [x, rest @ ..] => rest
+                .iter()
+                .map(|y| y.to_pixel_transform())
+                .fold(x.to_pixel_transform(), |acc, y| Box::new((acc, y))),
+        }
+    }
+
     fn run(self) -> anyhow::Result<()> {
         println!("Running with {:?}", self);
 
@@ -53,8 +77,17 @@ impl Args {
             .with_infinity_distance(self.infinity_distance)
             .with_max_iterations(self.max_iterations);
         let function = self.function.parse()?;
-        let image_buffer = self.backend.compile_and_render(&function, &params)?;
-        image_buffer.save(&self.output)?;
+        let iterations = self.backend.compile_and_render(&function, &params)?;
+        let transformed_luma = iterations.apply(self.luma_transform());
+
+        if let Some(palette) = self.palette {
+            transformed_luma
+                .apply(palette.to_palette())
+                .transform()
+                .save(&self.output)?;
+        } else {
+            transformed_luma.transform().save(&self.output)?;
+        }
         Ok(())
     }
 }
@@ -128,6 +161,97 @@ impl BackendName {
             #[cfg(feature = "vulkan_backend")]
             Self::Vulkan => julia_set::Vulkan.create_program(function)?.render(params)?,
         })
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+enum NamedPalette {
+    Tree,
+    Gold,
+    Snow,
+}
+
+impl FromStr for NamedPalette {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> anyhow::Result<Self> {
+        match s {
+            "tree" => Ok(Self::Tree),
+            "gold" => Ok(Self::Gold),
+            "snow" => Ok(Self::Snow),
+            _ => Err(anyhow!(
+                "Invalid palette. Use one of `tree`, `gold` or `snow`"
+            )),
+        }
+    }
+}
+
+impl NamedPalette {
+    fn to_palette(self) -> Palette<Rgb<u8>> {
+        match self {
+            Self::Tree => Palette::new(&[
+                Rgb([0, 0, 0]),
+                Rgb([170, 0, 0]),
+                Rgb([136, 204, 0]),
+                Rgb([170, 255, 136]),
+                Rgb([255, 255, 170]),
+                Rgb([255, 255, 255]),
+            ]),
+
+            Self::Gold => Palette::new(&[
+                Rgb([0, 0, 0]),
+                Rgb([187, 187, 0]),
+                Rgb([255, 187, 0]),
+                Rgb([255, 255, 0]),
+                Rgb([255, 255, 136]),
+                Rgb([255, 255, 255]),
+            ]),
+
+            Self::Snow => {
+                Palette::new(&[Rgb([0, 0, 0]), Rgb([136, 170, 255]), Rgb([255, 255, 255])])
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+enum NamedLumaTransform {
+    Negative,
+    Smoothstep,
+}
+
+impl FromStr for NamedLumaTransform {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> anyhow::Result<Self> {
+        match s {
+            "neg" | "negative" => Ok(Self::Negative),
+            "smooth" | "smoothstep" => Ok(Self::Smoothstep),
+            _ => Err(anyhow!("Invalid transform")),
+        }
+    }
+}
+
+impl NamedLumaTransform {
+    fn to_pixel_transform(self) -> Box<LumaTransform> {
+        match self {
+            Self::Negative => Box::new(Negative),
+            Self::Smoothstep => Box::new(Smoothstep),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct NamedLumaTransforms(Vec<NamedLumaTransform>);
+
+impl FromStr for NamedLumaTransforms {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> anyhow::Result<Self> {
+        s.split(',')
+            .map(|part| part.trim().parse::<NamedLumaTransform>())
+            .collect::<anyhow::Result<Vec<_>>>()
+            .map(Self)
     }
 }
 
