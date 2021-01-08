fc9b3d99afb7f3e260a6e21572b0a50778fe9109
Alex Ostrovski <ostrovski.alex@gmail.com>
2021-01-08 14:56:43 +0200

    Refactor Vulkan shader for performance
    - Use buffer instead of image
    - Specify local workgroup size

--- src/program.glsl
+++ src/program.glsl
@@ -86,27 +86,42 @@ vec2 compute(vec2 z) {
     COMPUTE
 }
 
-layout(set = 0, binding = 0, r8) uniform writeonly image2D img;
+// **NB.** Local sizes must correspond to ones declared in the `vulkan` module.
+layout(local_size_x = 16, local_size_y = 16) in;
+
+layout(set = 0, binding = 0) writeonly buffer Output {
+    uint[] data;
+} output_buffer;
+
 layout(set = 0, binding = 1) uniform Params {
     vec2 view_center;
     vec2 view_size;
+    uvec2 image_size;
     float inf_distance_sq;
     uint max_iterations;
 } params;
 
 void main() {
+    uvec2 image_size = params.image_size;
+    if (gl_GlobalInvocationID.x >= image_size.x || gl_GlobalInvocationID.y >= image_size.y) {
+        // We're out of image bounds, which happens if the image size is not divisible
+        // by the local workgroup size.
+        return;
+    }
+
     // Map coordinates to [0, 1].
-    vec2 pixel_pos = (gl_GlobalInvocationID.xy + vec2(0.5)) / vec2(imageSize(img));
+    vec2 pixel_pos = (gl_GlobalInvocationID.xy + vec2(0.5)) / vec2(image_size);
     // Map coordinates to [-0.5, 0.5], flip the imaginary coordinate and transform
     // per view bounds.
     vec2 z = (pixel_pos - vec2(0.5)) * vec2(1 , -1) * params.view_size + params.view_center;
 
     uint iter = params.max_iterations;
-    for (uint i = 0; i < MAX_ITERATIONS && i < params.max_iterations; i++) {
+    for (uint i = 0; i < MAX_ITERATIONS; i++) {
         z = compute(z);
 
         float z_sq_magnitude = z.x * z.x + z.y * z.y;
         if (
+            i >= params.max_iterations ||
             isnan(z_sq_magnitude) ||
             isinf(z_sq_magnitude) ||
             z_sq_magnitude > params.inf_distance_sq
@@ -117,5 +132,15 @@ void main() {
     }
 
     float color = float(iter) / params.max_iterations;
-    imageStore(img, ivec2(gl_GlobalInvocationID.xy), vec4(color));
+
+    uint output_index = image_size.x * gl_GlobalInvocationID.y + gl_GlobalInvocationID.x;
+    uint mask = uint(round(color * 255));
+    switch (output_index % 4) {
+        case 1: mask <<= 8; break;
+        case 2: mask <<= 16; break;
+        case 3: mask <<= 24; break;
+    }
+    output_index /= 4;
+
+    atomicOr(output_buffer.data[output_index], mask);
 }
