11ed0bbd4e539a8726e2e95986645ace8c308f67
slowli <slowli@users.noreply.github.com>
2024-08-04 10:43:25 +0000

    Deploying to gh-pages from @ slowli/julia-set-rs@90e20bc1b66e7c51aadb18e164c79c9654bf2b55 🚀

---
+++ trait.impl/core/error/trait.Error.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"julia_set":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"struct\" href=\"julia_set/struct.FnError.html\" title=\"struct julia_set::FnError\">FnError</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
